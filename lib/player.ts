7c92f2241f7718223a5194a334a431182f9bdb50
notedit <notedit@gmail.com>
2019-01-25 23:57:56 +0800

    add code

---
+++ lib/player.ts
@@ -0,0 +1,22 @@
+import { EventEmitter }   from 'events'
+import webrtc             from 'webrtc-adapter'
+
+
+class RTCPlayer extends EventEmitter {
+    
+    constructor() {
+        super()
+    }
+
+
+    startPlay(playUrl:string) {
+
+    }
+
+    stopPlay() {
+
+    }
+    
+}
+
+export default RTCPlayer
---
+++ lib/pusher.ts
@@ -0,0 +1,27 @@
+import { EventEmitter }   from 'events'
+import webrtc             from 'webrtc-adapter'
+
+
+class RTCPusher extends EventEmitter {
+
+    
+    constructor() {
+        super()
+    
+    }
+
+    startPush(pushUrl:string) {
+
+    }
+
+    stopPush() {
+
+    }
+
+    play(videoElement:HTMLVideoElement) {
+
+    }
+}
+
+export default RTCPusher
+

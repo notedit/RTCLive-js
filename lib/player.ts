3b92cdd3d8b5efa5928ba35ab7b98ae250adc7c3
notedit <notedit@gmail.com>
2019-01-26 22:11:07 +0800

    add pusher player

--- lib/player.ts
+++ lib/player.ts
@@ -2,19 +2,120 @@ import { EventEmitter }   from 'events'
 import webrtc             from 'webrtc-adapter'
 
 
+class RTCPlayerConfig {
+
+}
+
+
 class RTCPlayer extends EventEmitter {
-    
-    constructor() {
+
+    private stream:MediaStream
+    private peerconnection:RTCPeerConnection
+    private config:RTCPlayerConfig
+    private closed:boolean
+    private videoElement:HTMLVideoElement
+    private subscriberId:string
+
+    constructor(config:RTCPlayerConfig) {
         super()
+        this.config = config
     }
 
+    async startPlay(playUrl:string) {
 
-    startPlay(playUrl:string) {
+        return new Promise(async (resolve,reject) => {
+
+            // timeout for play 
+            let playTimeout = setTimeout(() => {
+                playTimeout = null
+                reject()
+            },2000)
+
+            let options = {
+                iceServers: [],
+                iceTransportPolicy : 'all',   // relay or all
+                bundlePolicy       : 'max-bundle',
+                rtcpMuxPolicy      : 'require',
+                sdpSemantics       : 'unified-plan'
+            }
+    
+            this.peerconnection = new RTCPeerConnection(options as RTCConfiguration)
+    
+            this.peerconnection.oniceconnectionstatechange = () => {
+                console.log(this.peerconnection.iceConnectionState)
+            }
+    
+            this.peerconnection.ontrack = (event:RTCTrackEvent) => {
+
+                if(playTimeout) {
+                    clearTimeout(playTimeout)
+                    playTimeout = null
+                }
+
+                setTimeout(() => {
+                    if (this.stream) {
+                        return
+                    }
+                    this.stream = event.streams[0]
+                }, 0)
+            }
+
+            this.peerconnection.addTransceiver("audio", {direction:"recvonly"})
+            this.peerconnection.addTransceiver("video", {direction:"recvonly"})
+
+            const offer = await this.peerconnection.createOffer()
+
+            await this.peerconnection.setLocalDescription(offer)
+
+            let res = await fetch(playUrl, {
+                method: 'post',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({
+                    sdp: offer.sdp
+                })
+            })
+    
+            let ret = await res.json()
+
+            this.subscriberId = ret.d.subscriberId 
+
+            let answer = new RTCSessionDescription({
+                type: 'answer',
+                sdp: ret.d.sdp
+            })
+
+            await this.peerconnection.setRemoteDescription(answer)
+        })
 
     }
 
-    stopPlay() {
+    async stopPlay(unplayUrl:string) {
 
+        let res = await fetch(unplayUrl, {
+            method: 'post',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                subscriberId:this.subscriberId
+            })
+        })
+
+        if (this.peerconnection) {
+            this.peerconnection.close()
+        }
+
+    }
+
+    play(videoElement:HTMLVideoElement) {
+
+        videoElement.setAttribute('playsinline','playsinline')
+        videoElement.setAttribute('autoplay', 'true')
+
+        videoElement.onloadedmetadata = () => {
+
+        }
+
+        this.videoElement = videoElement
+        this.videoElement.srcObject = this.stream
     }
     
 }
--- lib/pusher.ts
+++ lib/pusher.ts
@@ -2,25 +2,124 @@ import { EventEmitter }   from 'events'
 import webrtc             from 'webrtc-adapter'
 
 
+
+class RTCPusherConfig {
+
+}
+
 class RTCPusher extends EventEmitter {
 
-    
-    constructor() {
+    private stream:MediaStream
+    private peerconnection:RTCPeerConnection
+    private config:RTCPusherConfig
+    private closed:boolean
+    private videoElement:HTMLVideoElement
+
+    private audioTrack:MediaStreamTrack
+    private videoTrack:MediaStreamTrack 
+    private audioSender:RTCRtpSender
+    private videoSender:RTCRtpSender
+
+    constructor(config:RTCPusherConfig) {
         super()
-    
+        this.config = config
+        this.closed = false
     }
 
-    startPush(pushUrl:string) {
+    async setupLocalMedia() {
 
+        const stream = await navigator.mediaDevices.getUserMedia({
+            audio:true,
+            video:true
+        })
+        
+        this.audioTrack = stream.getAudioTracks()[0]
+        this.videoTrack = stream.getVideoTracks()[0]
+
+        this.stream = stream
     }
 
-    stopPush() {
+    async startPush(pushUrl:string) {
+
+        let options = {
+            iceServers: [],
+            iceTransportPolicy : 'all',   // relay or all
+            bundlePolicy       : 'max-bundle',
+            rtcpMuxPolicy      : 'require',
+            sdpSemantics       : 'unified-plan'
+        }
+
+        this.peerconnection = new RTCPeerConnection(options as RTCConfiguration)
+
+        this.peerconnection.oniceconnectionstatechange = () => {
+            console.log(this.peerconnection.iceConnectionState)
+        }
+
+        if (this.audioTrack) {
+            this.audioSender = await this.peerconnection.addTrack(this.audioTrack, this.stream)
+        }
+
+        if (this.videoTrack) {
+            this.videoSender = await this.peerconnection.addTrack(this.videoTrack, this.stream)
+        }
+
+        const offer = await this.peerconnection.createOffer()
+        // todo sdp mangle
+        await this.peerconnection.setLocalDescription(offer)
+
+        let res = await fetch(pushUrl, {
+            method: 'post',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                sdp: offer.sdp
+            })
+        })
+
+        let ret = await res.json()
+
+        let answer = new RTCSessionDescription({
+            type: 'answer',
+            sdp: ret.d.sdp
+        })
+
+        await this.peerconnection.setRemoteDescription(answer)
+    }
+
+    async stopPush(unpushUrl:string) {
+
+        if (this.audioSender) {
+            this.peerconnection.removeTrack(this.audioSender)
+        }
+        
+        if (this.videoSender) {
+            this.peerconnection.removeTrack(this.videoSender)
+        }
+
+        let res = await fetch(unpushUrl, {
+            method: 'post',
+            headers: { 'Content-Type': 'application/json' },
+        })
+        
+
+        if (this.peerconnection) {
+            this.peerconnection.close()
+        }
 
     }
 
     play(videoElement:HTMLVideoElement) {
 
+        videoElement.setAttribute('playsinline','playsinline')
+        videoElement.setAttribute('autoplay', 'true')
+
+        videoElement.onloadedmetadata = () => {
+
+        }
+
+        this.videoElement = videoElement
+        this.videoElement.srcObject = this.stream
     }
+
 }
 
 export default RTCPusher
